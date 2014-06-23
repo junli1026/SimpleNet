525814dbfac53f3f260559ba1db563a007ea5a6f
Jun <junli1026@gmail.com>
2014-06-23 16:35:07 -0400

    Update subscriber.cpp

--- src2/publish_subscribe/subscriber.cpp
+++ src2/publish_subscribe/subscriber.cpp
@@ -1,9 +1,29 @@
-#include "../tcp_client.h"
+#include "subscriber.h"
+#include "../buffer.h"
 
 namespace simple{
+Subscriber::Subscriber(){}
 
-class Subscriber: public TcpClient{
+Subscriber::~Subscriber(){};
 
-};
+void Subscriber::subscribe(const char* host, int port){
+	int fd = this->connector_.doConnect(host, port);
+	this->fd_ = fd;	
+}
+
+std::vector<uint8_t> Subscriber::receiveData(){
+	Buffer buf;
+	std::vector<uint8_t> ret;
+	uint8_t b[1024];
+	int n;
+	while((n = read(this->fd_, b, 1024)) > 0){
+		buf.append(b, n);
+		ret = buf.retrieveBy("\r\n", 2);
+		if(ret.size() != 0){
+			break;
+		}
+	}
+	return ret;
+}
 
 }
