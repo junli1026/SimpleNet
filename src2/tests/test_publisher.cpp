56904955290e33ed3e26c115eb9b6bbe76e21d32
Jun <junli1026@gmail.com>
2014-06-23 13:10:05 -0400

    Update test_publisher.cpp

--- src2/tests/test_publisher.cpp
+++ src2/tests/test_publisher.cpp
@@ -1,4 +1,4 @@
-#include "../publish_subscribe/publisher.cpp"
+#include "../publish_subscribe/publisher.h"
 
 #include <string>
 #include <iostream>
@@ -8,23 +8,7 @@
 
 using namespace simple;
 
-void *run(void* arg){
-	Publisher* p = static_cast<Publisher*>(arg);
-	p->runThreading();
-}
 
 int main(){
-	Publisher p;
-	p.monitor("127.0.0.1", 8001);
-
-	pthread_t id;
-	int ret = pthread_create(&id, NULL, run, &p);
-	assert(ret == 0);
-
-	std::string a;
-	while(true){
-		std::getline (std::cin,a);
-		p.publish(a.c_str(), strlen(a.c_str()));
-	}
-
+	Publisher p("127.0.0.1", 8001);
 }
