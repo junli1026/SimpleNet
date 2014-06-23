7ce1e50b1daf5eebd963119929abfb5b5a6e71e5
Jun <junli1026@gmail.com>
2014-06-23 16:36:18 -0400

    Update test_publisher.cpp

--- src2/tests/test_publisher.cpp
+++ src2/tests/test_publisher.cpp
@@ -4,11 +4,24 @@
 #include <iostream>
 #include <pthread.h>
 #include <assert.h>
-
+#include <string>
 
 using namespace simple;
 
 
 int main(){
 	Publisher p("127.0.0.1", 8001);
+	p.runService();
+
+	std::string a;
+	while(true){
+		std::getline(std::cin, a);
+		if(a == std::string("quit")){
+			break;
+		}else{
+			p.publish(a.c_str(), strlen(a.c_str()));
+		}
+	}
+	p.stopService();
+	sleep(1);
 }
