490ba1917ed3e4f3bd51a49e4241e6d7357b84d5
Jun <junli1026@gmail.com>
2014-06-26 14:49:19 -0400

    Update eventloop.h

--- src/eventloop.h
+++ src/eventloop.h
@@ -1,16 +1,35 @@
-#ifndef _EVNET_LOOP_H_
-#define _EVNET_LOOP_H_
+#ifndef _EVENT_LOOP_H_
+#define _EVENT_LOOP_H_
 
+#include <functional>
+#include <memory>
 namespace simple{
 class EventLoop{
 
 private:
-	Channel channel_;
+	std::shared_ptr<Channel> channelptr_;
+	std::shared_ptr<Connector> connectorptr_;
+	std::shared_ptr<Acceptor> acceptorptr_;
+	std::shared_ptr<IOHandler> iohandlerptr_;
+	bool hasPipeEvent();
+	bool hasSocketEvent();
+	
+	std::function<void(Connection&)> acceptcb_;
+	std::function<void(Connection&)> readcb_;
+	std::function<void(Connection&)> writecb_;
+	std::function<void(Connection&)> connectcb_;
+	EventLoop& operator=(const EventLoop& el){}
+	EventLool(const EventLoop& el){}
 public:
 	EventLoop(Poller& p, Connector& c, Acceptor& a, IOHandler& h);
 
 	
-	void loop();
+	void loop(){
+		while(true){
+				
+			
+		}
+	}
 };
 
 }
