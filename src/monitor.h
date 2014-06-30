4fcca9c9d818a84ba63b08d4fbd3ce48ed268252
Jun <junli1026@gmail.com>
2014-06-30 11:10:02 -0400

    Update monitor.h

--- src/monitor.h
+++ src/monitor.h
@@ -4,39 +4,29 @@
 #include <memory>
 #include <set>
 #include "poller.h"
-#include "channel.h"
+#include "event.h"
 #include "connection.h"
 
 namespace simple{
 
-enum EventType{
-	EPublish,
-	EAccept,
-	EConnect,
-	ERead,
-	EWrite
-};
-
 class Monitor{
 private:
 	std::set<int> acceptFds_;
-	std::shared_ptr<ConnectionMap> connMap_;
 	std::shared_ptr<Poller> poller_;
-	std::shared_ptr<Channel> channel_;
+	bool isAcceptFd(int fd);
 	Monitor& operator=(const Monitor& m){}
 	Monitor(const Monitor& m){}
-
+	
 public:
-	Monitor(std::shared_ptr<Poller> p, 
-			std::shared_ptr<Channel> c, 
-			std::shared_ptr<ConnectionMap> conns);
+	Monitor(std::shared_ptr<Poller> p);
 	~Monitor();
 	void monitorAccept(int fd);
 	void monitorRead(std::shared_ptr<Connection> cptr);
 	void monitorWrite(std::shared_ptr<Connection> cptr);
 	void stopMonitor(std::shared_ptr<Connection> cptr);
-	void hasEvent();
-	EventType nextEvent();
+
+	bool hasEvent();
+	Event<int> nextEvent(); //Event's data is file descriptor
 };
 
 }
