a06ab2730468e83341b4e59dfdaaf36a3f4001d6
Jun <junli1026@gmail.com>
2014-06-30 11:06:26 -0400

    Update monitor.cpp

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -1,16 +1,14 @@
 #include "monitor.h"
+#include <sys/epoll.h>
+#include <assert.h>
 
 namespace simple{
 
-Monitor::Monitor(std::shared_ptr<Poller> p, 
-				std::shared_ptr<Channel> c
-				std::shared_ptr<ConnectionMap> m){
+Monitor::Monitor(std::shared_ptr<Poller> p){
 	this->poller_ = p;
-	this->channel_ = c;
-	this->connMap_ = m;
 }
 
-~Monitor(){
+Monitor::~Monitor(){
 }
 
 void Monitor::monitorAccept(int fd){
@@ -61,7 +59,36 @@ void Monitor::stopMonitor(std::shared_ptr<Connection> cptr){
 	this->poller_->remove(cptr->fd());
 }
 
+bool Monitor::hasEvent(){
+	if(this->poller_->hasEvent()){
+		return true;
+	}else{
+		this->poller_->poll(200);
+		return this->poller_->hasEvent();
+	}
+}
 
+bool Monitor::isAcceptFd(int fd){
+	auto it = this->acceptFds_.find(fd);
+	return (it != this->acceptFds_.end());
+}
 
+Event<int> Monitor::nextEvent(){
+	if(this->hasEvent()){
+		struct epoll_event* ev = this->poller_->nextEvent();
+		assert(ev);
+		int fd = ev->data.fd;
+		if(this->isAcceptFd(fd)){
+			return Event<int>(GL_EventAccept, fd);
+		}else if(ev->events & EPOLLIN){
+			return Event<int>(GL_EventRead, fd);
+		}else{
+			assert(ev->events & EPOLLOUT);
+			return Event<int>(GL_EventRead, fd);
+		}
+	}else{
+		return Event<int>(GL_EventNone, -1);
+	}
+}
 
 }
